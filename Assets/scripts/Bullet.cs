795a61eed870e79022b12c3036be0e3cd88d9edf
Ole Korff <o.korff@gmail.com>
2014-09-18 16:52:19 +0900

    Player, Bullets and Background added

---
+++ Assets/scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+    public float speed;
+
+    void Start() {
+    }
+
+	
+	void Update () {
+        float dt = Time.deltaTime;
+        float s = dt * speed;
+
+        transform.position = transform.position + new Vector3(0, s, 0);
+
+        if (transform.position.y > 2) {
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    public GameObject bullet;
+    public float speed;	
+	
+    void Start () {
+	}
+	
+	void Update () {
+        float dt = Time.deltaTime;
+        float s = dt * speed;
+
+        if (Input.GetKey("a")) {
+            transform.position = transform.position + new Vector3(-s, 0, 0);
+        }
+        if (Input.GetKey("d")) {
+            transform.position = transform.position + new Vector3(s, 0, 0);
+        }
+
+        if (Input.GetKeyDown("space")) {
+            Instantiate(bullet, transform.position + new Vector3(-0.05f, 0, 0), Quaternion.identity);
+            Instantiate(bullet, transform.position + new Vector3(0.05f, 0, 0), Quaternion.identity);
+        }
+	}
+}
