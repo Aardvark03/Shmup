88285f7339530fb2728d673640884a9981c33059
Ole Korff <o.korff@gmail.com>
2014-09-24 14:48:57 +0900

    Android controls working

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -8,24 +8,47 @@ public class Player : MonoBehaviour {
     public delegate void GameOver();
     public static event GameOver OnGameOver;
 
-	
-	void Update () {
+    Vector3 getDistanceToMove() {
+        Vector3 distanceToMove = new Vector3(0f, 0f, 0f);
+    
         float dt = Time.deltaTime;
         float s = dt * speed;
 
+        if (Application.platform == RuntimePlatform.Android) {
+            float acceleration = Input.acceleration.x;
+
+            if (acceleration > -0.1f && acceleration < 0.1f) {
+                return distanceToMove;
+            }
+
+            if (acceleration > 0) {
+                distanceToMove.x += s;
+            } else {
+                distanceToMove.x -= s;
+            }
+        } else {
+            if (Input.GetKey("a")) {
+                distanceToMove.x -= s;
+            }
+        
+            if (Input.GetKey("d")) {
+                distanceToMove.x += s; 
+            }
+        }
+
+        return distanceToMove;
+    }
+
+	
+	void Update () {
         Rect worldBounds = GameController.worldBounds;
         float width = renderer.bounds.extents.x;
 
         Vector3 currentPosition = rigidbody2D.position;
         Vector3 targetPosition = currentPosition;
 
-        if (Input.GetKey("a")) {
-            targetPosition += new Vector3(-s, 0, 0);
-        }
-        if (Input.GetKey("d")) {
-            targetPosition += new Vector3(s, 0, 0);
-        }
-
+        targetPosition += getDistanceToMove();
+        
         if (targetPosition.x - width < worldBounds.x || targetPosition.x + width > worldBounds.x + worldBounds.width) {
             targetPosition = currentPosition;
         }
@@ -37,6 +60,14 @@ public class Player : MonoBehaviour {
         if (Input.GetKeyDown("space")) {
             Instantiate(bullet, transform.position + new Vector3(-0.05f, 0, 0), Quaternion.identity);
             Instantiate(bullet, transform.position + new Vector3(0.05f, 0, 0), Quaternion.identity);
+        }
+
+         foreach (Touch touch in Input.touches) {
+            if (touch.phase == TouchPhase.Ended) {
+                Instantiate(bullet, transform.position + new Vector3(-0.05f, 0, 0), Quaternion.identity);
+                Instantiate(bullet, transform.position + new Vector3(0.05f, 0, 0), Quaternion.identity);
+   
+            }
         }
 	}
 
